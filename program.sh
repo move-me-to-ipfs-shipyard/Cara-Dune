55e0cd8e46586785f76d0009a893f9d777979768
Cara-Dune <Cara-Dune@i-think-that-did-it>
2022-05-15 09:04:31 +0300

    i rename aliases in deps.edn to :Ripley :Genie :Zazu :James-Acaster because at that time i looks like i overreacted

--- program.sh
+++ program.sh
@@ -3,7 +3,7 @@
 repl(){
   clj \
     -J-Dclojure.core.async.pool-size=1 \
-    -X:repl Ripley.core/process \
+    -X:Ripley Ripley.core/process \
     :main-ns Cara-Dune.main
 }
 
@@ -26,7 +26,7 @@ tag(){
 jar(){
 
   clojure \
-    -X:identicon Zazu.core/process \
+    -X:Zazu Zazu.core/process \
     :word '"Cara-Dune"' \
     :filename '"out/identicon/icon.png"' \
     :size 256
@@ -35,7 +35,7 @@ jar(){
   COMMIT_HASH=$(git rev-parse --short HEAD)
   COMMIT_COUNT=$(git rev-list --count HEAD)
   clojure \
-    -X:uberjar Genie.core/process \
+    -X:Genie Genie.core/process \
     :main-ns Cara-Dune.main \
     :filename "\"out/Cara-Dune-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
     :paths '["src"]'
