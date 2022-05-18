9822772ec2a2cf8029c458e37199e865c49a0181
Cara-Dune <Cara-Dune@i-think-that-did-it>
2022-05-18 12:12:58 +0300

    i move http server and http-api pubsub to seed namespaces :David-Mitchell there's something about me that elephants like

--- src/Cara_Dune/main.clj
+++ src/Cara_Dune/main.clj
@@ -380,98 +380,8 @@
 
     (go
       (loop []
-        (when-let [{:keys [message from] :as value} (<! sub|)]
-          (condp = (:op message)
-            :game-state
-            (let [{:keys [game-state]} message]
-              (swap! gameA merge game-state))
-            :player-state
-            (let [{:keys [game-state]} message]
-              (swap! gameA update-in [:players from] merge message))
-            :games
-            (let [{:keys [frequency host-peer-id]} message]
-              (swap! gamesA update-in [frequency] merge message)))
-          (recur))))
-
-    (go
-      (loop []
-        (<! (timeout 3000))
-        (let [expired (into []
-                            (comp
-                             (keep (fn [[frequency {:keys [timestamp]}]]
-                                     #_(println (- (.getTime (java.util.Date.)) timestamp))
-                                     (when-not (< (- (.getTime (java.util.Date.)) timestamp) 4000)
-                                       frequency))))
-                            @gamesA)]
-          (when-not (empty? expired)
-            (apply swap! gamesA dissoc expired)))
-        (recur)))
-
-    (go
-      (loop []
-        (<! (timeout 3000))
-        (let [expired (into []
-                            (comp
-                             (keep (fn [[frequency {:keys [timestamp peer-id]}]]
-                                     #_(println (- (.getTime (java.util.Date.)) timestamp))
-                                     (when-not (< (- (.getTime (java.util.Date.)) timestamp) 4000)
-                                       frequency))))
-                            (:players @gameA))]
-          (when-not (empty? expired)
-            (apply swap! gameA update :players dissoc expired)))
-        (recur)))
-
-    (go
-      (loop []
         (when-let [value (<! ops|)]
           (condp = (:op value)
-            :game
-            (let [{:keys [frequency role]} value
-                  id| (chan 1)
-                  port (or (System/getenv "CARA_DUNE_IPFS_PORT") "5001")
-                  ipfs-api-url (format "http://127.0.0.1:%s" port)
-                  games-topic (Cara-Dune.corn/encode-base64url-u "raisins")
-                  game-topic (Cara-Dune.corn/encode-base64url-u frequency)
-                  _ (Cara-Dune.corn/subscribe-process
-                     {:sub| sub|
-                      :cancel| cancel-sub|
-                      :frequency frequency
-                      :ipfs-api-url ipfs-api-url
-                      :ipfs-api-multiaddress (format "/ip4/127.0.0.1/tcp/%s" port)
-                      :id| id|})
-                  host? (= role :host)
-                  {:keys [peer-id]} (<! id|)]
-              #_(println :game value)
-              (go
-                (loop []
-                  (alt!
-                    cancel-pub|
-                    ([_] (do nil))
-
-                    (timeout 2000)
-                    ([_]
-                     (when host?
-                       (Cara-Dune.corn/pubsub-pub
-                        ipfs-api-url games-topic (str {:op :games
-                                                       :timestamp (.getTime (java.util.Date.))
-                                                       :frequency frequency
-                                                       :host-peer-id peer-id}))
-                       (Cara-Dune.corn/pubsub-pub
-                        ipfs-api-url game-topic (str {:op :game-state
-                                                      :timestamp (.getTime (java.util.Date.))
-                                                      :game-state {:host-peer-id peer-id}})))
-
-                     (Cara-Dune.corn/pubsub-pub
-                      ipfs-api-url game-topic (str {:op :player-state
-                                                    :timestamp (.getTime (java.util.Date.))
-                                                    :peer-id peer-id}))
-                     (recur))))))
-
-            :leave
-            (let [{:keys [frequency]} value]
-              (>! cancel-sub| true)
-              (>! cancel-pub| true)
-              (reset! gameA {}))
 
             :discover
             (let [discover-jframe (JFrame. "discover")]
@@ -495,33 +405,7 @@
 
             :settings-value
             (let []
-              (swap! settingsA merge value))
+              (swap! settingsA merge value)))
 
-            :host-yes
-            (let [{:keys [frequency]} value]
-              (println :frequency frequency)))
-
-          (recur))))
-
-    (let [port (or (System/getenv "CARA_DUNE_IPFS_PORT") "5001")
-          ipfs-api-url (format "http://127.0.0.1:%s" port)
-          id| (chan 1)
-          raw-stream-connection-pool (Simba.http/connection-pool {:connection-options {:raw-stream? true}})]
-      
-      (alter-var-root #'raw-stream-connection-pool (constantly raw-stream-connection-pool))
-      (Cara-Dune.corn/subscribe-process
-       {:sub| sub|
-        :raw-stream-connection-pool raw-stream-connection-pool
-        :cancel| (chan (sliding-buffer 1))
-        :frequency "raisins"
-        :ipfs-api-url ipfs-api-url
-        :ipfs-api-multiaddress (format "/ip4/127.0.0.1/tcp/%s" port)
-        :id| id|}))
-
-    (let [port (or (try (Integer/parseInt (System/getenv "PORT"))
-                        (catch Exception e nil))
-                   3344)]
-      (Cara-Dune.microwaved-potatoes/process
-       {:port port
-        :host| host|})))
+          (recur)))))
   (println "Kuiil has spoken"))
