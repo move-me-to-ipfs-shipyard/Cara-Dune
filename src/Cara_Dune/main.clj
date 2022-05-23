e7dddf02ca458bfde0510f66bb44b3a7cc310b50
Cara-Dune <Cara-Dune@i-think-that-did-it>
2022-05-23 20:50:25 +0300

    i move all code to main :Eminem Pete! :Pete-Davidson ah? :Eminem don't fuckin' do it again :Pete got it

--- src/Cara_Dune/main.clj
+++ src/Cara_Dune/main.clj
@@ -13,13 +13,27 @@
    [clojure.repl :as Wichita.repl]
 
    [aleph.http :as Simba.http]
+   [manifold.deferred :as Nala.deferred]
+   [manifold.stream :as Nala.stream]
+   [byte-streams :as Rafiki]
+   [cheshire.core :as Cheshire-Cat.core]
 
    [Cara-Dune.seed]
+   [Cara-Dune.host]
+   [Cara-Dune.raisins]
+   [Cara-Dune.peanuts]
+   [Cara-Dune.kiwis]
+   [Cara-Dune.salt]
    [Cara-Dune.microwaved-potatoes]
    [Cara-Dune.corn]
    [Cara-Dune.beans])
   (:import
-   (java.io File))
+   (java.io File)
+   (io.ipfs.api IPFS)
+   (java.util.stream Stream)
+   (java.util Base64)
+   (java.io BufferedReader)
+   (java.nio.charset StandardCharsets))
   (:gen-class))
 
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
@@ -31,10 +45,262 @@
   []
   (require
    '[Cara-Dune.seed]
+   '[Cara-Dune.host]
+   '[Cara-Dune.raisins]
+   '[Cara-Dune.peanuts]
+   '[Cara-Dune.kiwis]
+   '[Cara-Dune.salt]
    '[Cara-Dune.microwaved-potatoes]
+   '[Cara-Dune.corn]
    '[Cara-Dune.beans]
-   '[Cara-Dune.main]
    :reload))
+
+(defn encode-base64url-u
+  [^String string]
+  (-> (Base64/getUrlEncoder) (.withoutPadding)
+      (.encodeToString (.getBytes string StandardCharsets/UTF_8)) (->> (str "u"))))
+
+(defn decode-base64url-u
+  [^String string]
+  (-> (Base64/getUrlDecoder)
+      (.decode (subs string 1))
+      (String. StandardCharsets/UTF_8)))
+
+(defn pubsub-sub
+  [base-url topic message| cancel| raw-stream-connection-pool]
+  (let [streamV (volatile! nil)]
+    (->
+     (Nala.deferred/chain
+      (Simba.http/post (str base-url "/api/v0/pubsub/sub")
+                       {:query-params {:arg topic}
+                        :pool raw-stream-connection-pool})
+      :body
+      (fn [stream]
+        (vreset! streamV stream)
+        stream)
+      #(Nala.stream/map Rafiki/to-string %)
+      (fn [stream]
+        (Nala.deferred/loop
+         []
+          (->
+           (Nala.stream/take! stream :none)
+           (Nala.deferred/chain
+            (fn [message-string]
+              (when-not (identical? message-string :none)
+                (let [message (Cheshire-Cat.core/parse-string message-string true)]
+                  #_(println :message message)
+                  (put! message| message))
+                (Nala.deferred/recur))))
+           (Nala.deferred/catch Exception (fn [ex] (println ex)))))))
+     (Nala.deferred/catch Exception (fn [ex] (println ex))))
+
+    (go
+      (<! cancel|)
+      (Nala.stream/close! @streamV))
+    nil))
+
+(defn pubsub-pub
+  [base-url topic message]
+  (let []
+
+    (->
+     (Nala.deferred/chain
+      (Simba.http/post (str base-url "/api/v0/pubsub/pub")
+                       {:query-params {:arg topic}
+                        :multipart [{:name "file" :content message}]})
+      :body
+      Rafiki/to-string
+      (fn [response-string] #_(println :repsponse reresponse-stringsponse)))
+     (Nala.deferred/catch
+      Exception
+      (fn [ex] (println ex))))
+
+    nil))
+
+(defn subscribe-process
+  [{:keys [^String ipfs-api-multiaddress
+           ^String ipfs-api-url
+           frequency
+           raw-stream-connection-pool
+           sub|
+           cancel|
+           id|]
+    :as opts}]
+  (let [ipfs (IPFS. ipfs-api-multiaddress)
+        base-url ipfs-api-url
+        topic (encode-base64url-u frequency)
+        id (-> ipfs (.id) (.get "ID"))
+        message| (chan (sliding-buffer 10))]
+    (put! id| {:peer-id id})
+    (pubsub-sub base-url  topic message| cancel| raw-stream-connection-pool)
+
+    (go
+      (loop []
+        (when-let [value (<! message|)]
+          (put! sub| (merge value
+                            {:message (-> (:data value) (decode-base64url-u) (read-string))}))
+          #_(println (merge value
+                            {:message (-> (:data value) (decode-base64url-u) (read-string))}))
+          #_(when-not (= (:from value) id)
+
+              #_(println (merge value
+                                {:message (-> (:data value) (decode-base64url-u) (read-string))})))
+          (recur))))
+
+    #_(go
+        (loop []
+          (<! (timeout 2000))
+          (pubsub-pub base-url topic (str {:id id
+                                           :rand-int (rand-int 100)}))
+          (recur)))))
+
+
+(comment
+
+  (go
+    (loop []
+      (when-let [{:keys [message from] :as value} (<! sub|)]
+        (condp = (:op message)
+          :game-state
+          (let [{:keys [game-state]} message]
+            (swap! gameA merge game-state))
+          :player-state
+          (let [{:keys [game-state]} message]
+            (swap! gameA update-in [:players from] merge message))
+          :games
+          (let [{:keys [frequency host-peer-id]} message]
+            (swap! gamesA update-in [frequency] merge message)))
+        (recur))))
+
+  (go
+    (loop []
+      (<! (timeout 3000))
+      (let [expired (into []
+                          (comp
+                           (keep (fn [[frequency {:keys [timestamp]}]]
+                                   #_(println (- (.getTime (java.util.Date.)) timestamp))
+                                   (when-not (< (- (.getTime (java.util.Date.)) timestamp) 4000)
+                                     frequency))))
+                          @gamesA)]
+        (when-not (empty? expired)
+          (apply swap! gamesA dissoc expired)))
+      (recur)))
+
+  (go
+    (loop []
+      (<! (timeout 3000))
+      (let [expired (into []
+                          (comp
+                           (keep (fn [[frequency {:keys [timestamp peer-id]}]]
+                                   #_(println (- (.getTime (java.util.Date.)) timestamp))
+                                   (when-not (< (- (.getTime (java.util.Date.)) timestamp) 4000)
+                                     frequency))))
+                          (:players @gameA))]
+        (when-not (empty? expired)
+          (apply swap! gameA update :players dissoc expired)))
+      (recur)))
+
+  (go
+    (loop []
+      (when-let [value (<! ops|)]
+        (condp = (:op value)
+          :game
+          (let [{:keys [frequency role]} value
+                id| (chan 1)
+                port (or (System/getenv "Jar_Jar_IPFS_PORT") "5001")
+                ipfs-api-url (format "http://127.0.0.1:%s" port)
+                games-topic (Cara-Dune.corn/encode-base64url-u "raisins")
+                game-topic (Cara-Dune.corn/encode-base64url-u frequency)
+                _ (Cara-Dune.corn/subscribe-process
+                   {:sub| sub|
+                    :cancel| cancel-sub|
+                    :frequency frequency
+                    :ipfs-api-url ipfs-api-url
+                    :ipfs-api-multiaddress (format "/ip4/127.0.0.1/tcp/%s" port)
+                    :id| id|})
+                host? (= role :host)
+                {:keys [peer-id]} (<! id|)]
+            #_(println :game value)
+            (go
+              (loop []
+                (alt!
+                  cancel-pub|
+                  ([_] (do nil))
+
+                  (timeout 2000)
+                  ([_]
+                   (when host?
+                     (Cara-Dune.corn/pubsub-pub
+                      ipfs-api-url games-topic (str {:op :games
+                                                     :timestamp (.getTime (java.util.Date.))
+                                                     :frequency frequency
+                                                     :host-peer-id peer-id}))
+                     (Cara-Dune.corn/pubsub-pub
+                      ipfs-api-url game-topic (str {:op :game-state
+                                                    :timestamp (.getTime (java.util.Date.))
+                                                    :game-state {:host-peer-id peer-id}})))
+
+                   (Cara-Dune.corn/pubsub-pub
+                    ipfs-api-url game-topic (str {:op :player-state
+                                                  :timestamp (.getTime (java.util.Date.))
+                                                  :peer-id peer-id}))
+                   (recur))))))
+
+          :leave
+          (let [{:keys [frequency]} value]
+            (>! cancel-sub| true)
+            (>! cancel-pub| true)
+            (reset! gameA {}))
+
+          :discover
+          (let [discover-jframe (JFrame. "discover")]
+            (Cara-Dune.kiwis/discover-process
+             {:jframe discover-jframe
+              :root-jframe jframe
+              :ops| ops|
+              :gamesA gamesA
+              :gameA gameA
+              :stateA stateA})
+            (reset! gameA @gameA))
+
+          :settings
+          (let [settings-jframe (JFrame. "settings")]
+            (Cara-Dune.kiwis/settings-process
+             {:jframe settings-jframe
+              :root-jframe jframe
+              :ops| ops|
+              :settingsA settingsA})
+            (reset! settingsA @settingsA))
+
+          :settings-value
+          (let []
+            (swap! settingsA merge value))
+
+          :host-yes
+          (let [{:keys [frequency]} value]
+            (println :frequency frequency)))
+
+        (recur))))
+
+
+
+  (let [port (or (System/getenv "Jar_Jar_IPFS_PORT") "5001")
+        ipfs-api-url (format "http://127.0.0.1:%s" port)
+        id| (chan 1)
+        raw-stream-connection-pool (Simba.http/connection-pool {:connection-options {:raw-stream? true}})]
+
+    (alter-var-root #'raw-stream-connection-pool (constantly raw-stream-connection-pool))
+    (Cara-Dune.corn/subscribe-process
+     {:sub| sub|
+      :raw-stream-connection-pool raw-stream-connection-pool
+      :cancel| (chan (sliding-buffer 1))
+      :frequency "raisins"
+      :ipfs-api-url ipfs-api-url
+      :ipfs-api-multiaddress (format "/ip4/127.0.0.1/tcp/%s" port)
+      :id| id|}))
+
+  ;
+  )
 
 (defn -main
   [& args]
--- src/Cara_Dune/ui.cljs
+++ src/Cara_Dune/ui.cljs
@@ -12,21 +12,221 @@
    [goog.string.format :as format]
    [goog.string :refer [format]]
    [goog.object]
+   [goog.events]
    [cljs.reader :refer [read-string]]
 
    ["react" :as Pacha]
    ["react-dom/client" :as Pacha.dom.client]
    [reagent.core :as Kuzco.core]
+   [reagent.dom :as Kuzco.dom]
 
-   [Cara-Dune.raisins]
-   [Cara-Dune.peanuts]
-   [Cara-Dune.kiwis]
-   #_[Cara-Dune.salt]))
+   [reitit.frontend :as Yzma.frontend]
+   [reitit.frontend.easy :as Yzma.frontend.easy]
+   [reitit.coercion.spec :as Yzma.coercion.spec]
+   [reitit.frontend.controllers :as Yzma.frontend.controllers]
+   [reitit.frontend.history :as Yzma.frontend.history]
+   [spec-tools.data-spec :as Yzma.data-spec]
+
+   ["antd/lib/layout" :default ThemeSongGuyLayout]
+   ["antd/lib/menu" :default ThemeSongGuyMenu]
+   ["antd/lib/button" :default ThemeSongGuyButton]
+   ["antd/lib/row" :default ThemeSongGuyRow]
+   ["antd/lib/col" :default ThemeSongGuyCol]
+   ["antd/lib/input" :default ThemeSongGuyInput]
+   ["antd/lib/table" :default ThemeSongGuyTable]
+
+   [clojure.test.check.generators :as Pawny.generators]
+   [clojure.spec.alpha :as Wichita.spec]
+
+   ["konva/lib/shapes/Rect"]
+   ["konva" :default Konva]
+   ["react-konva" :as ReactKonva :rename {Stage KonvaStage
+                                          Layer KonvaLayer
+                                          Rect KonvaRect
+                                          Path KonvaPath
+                                          Circle KonvaCircle
+                                          Group KonvaGroup
+                                          Wedge KonvaWedge
+                                          RegularPolygon KonvaRegularPolygon}]
+
+   ["@react-spring/web" :as ReactSpring :rename {animated ReactSpringAnimated
+                                                 Spring ReactSpringSpring}]))
 
 (defonce matchA (Kuzco.core/atom nil))
 (defonce stateA (Kuzco.core/atom {}))
 (defonce ops| (chan (sliding-buffer 10)))
 (defonce Pacha-dom-root (Pacha.dom.client/createRoot (.getElementById js/document "ui")))
+
+
+
+(def ^:const box-size 32)
+(def ^:const rows 16)
+(def ^:const cols 16)
+
+(def colors
+  {:sands "#edd3af" #_"#D2B48Cff"
+   :Korvus "lightgrey"
+   :signal-tower "brown"
+   :recharge "#30ad23"
+   :Cara-Dune "blue"})
+
+(defn rc-background-layer
+  []
+  (Kuzco.core/with-let
+    []
+    [:> (.-Layer ReactKonva)
+     {:id "background-layer"}
+     [:> (.-Rect ReactKonva) {:width (* box-size cols)
+                              :height (* box-size rows)
+                              :id "background-rect"
+                              :x 0
+                              :y 0
+                              :fill "#ffffff"
+                              :strokeWidth 0
+                              :stroke "white"}]]))
+
+(defn rc-game
+  [match stateA ops|]
+  (Kuzco.core/with-let
+    []
+    [:> (.-Stage ReactKonva)
+     {:width (* box-size cols)
+      :height (* box-size rows)}
+     [rc-background-layer]]))
+
+(defn canvas-process
+  [{:keys []
+    :as opts}]
+  (let []))
+
+(defn rc-main-page
+  [match stateA ops|]
+  [:> (.-Content ThemeSongGuyLayout)
+   {:style {:background-color "white"}}
+   [:div {}
+    [:div ":Co-Pilot i saw your planet destroyed - i was on the Death Star"]
+    [:div ":_ which one?"]]])
+
+
+(defn rc-game-page
+  [match stateA ops|]
+  [:> (.-Content ThemeSongGuyLayout)
+   {:style {:background-color "white"}}
+   [:> ThemeSongGuyRow
+    #_[:> ThemeSongGuyCol
+       {:flex 1}
+       [Cara-Dune.salt/rc-editor]]
+    [:> ThemeSongGuyCol
+     {:flex 1}
+     [rc-game match stateA ops|]]]])
+
+(defn rc-settings-page
+  [match stateA ops|]
+  [:> (.-Content ThemeSongGuyLayout)
+   {:style {:background-color "white"}}
+   [:> ThemeSongGuyRow
+    "settings"
+    #_(str "settings" (:rand-int @stateA))]])
+
+(defn rc-current-page
+  [matchA stateA ops|]
+  (Kuzco.core/with-let
+    [route-keyA (Kuzco.core/cursor matchA [:data :name])]
+    (let [route-key @route-keyA]
+      [:> ThemeSongGuyLayout
+       [:> ThemeSongGuyMenu
+        {:mode "horizontal"
+         :size "large"
+         :selectedKeys [route-key]
+         :items [{:type "item"
+                  :label
+                  (Kuzco.core/as-element [:a {:href (Yzma.frontend.easy/href :rc-main-page)} "program"])
+                  :key :rc-main-page
+                  :icon nil}
+                 {:type "item"
+                  :label
+                  (Kuzco.core/as-element [:a {:href (Yzma.frontend.easy/href :rc-game-page)} "game"])
+                  :key :rc-game-page
+                  :icon nil}
+                 {:type "item"
+                  :label
+                  (Kuzco.core/as-element [:a {:href (Yzma.frontend.easy/href :rc-settings-page)} "settings"])
+                  :key :rc-settings-page
+                  :icon nil}]
+         #_[{:label "program"
+             :key "program"
+             :icon nil
+             :children []}]}]
+       (when-let [match @matchA]
+         [(-> match :data :view) match stateA])])
+    #_[:<>
+
+       [:ul
+        [:li [:a {:href (Yzma.frontend.easy/href :rc-main-page)} "game"]]
+        [:li [:a {:href (Yzma.frontend.easy/href :rc-settings-page)} "settings"]]]
+       (when-let [match @matchA]
+         [(-> match :data :view) match stateA])]))
+
+(defn ui-process
+  [{:keys [Pacha-dom-root
+           matchA
+           stateA
+           ops|]
+    :as opts}]
+  (let [history (Yzma.frontend.easy/start!
+                 (Yzma.frontend/router
+                  ["/"
+                   [""
+                    {:name :rc-main-page
+                     :view rc-main-page
+                     :controllers [{:start (fn [_]
+                                             (js/console.log "start rc-main-page"))
+                                    :stop (fn [_]
+                                            (js/console.log "stop rc-main-page"))}]}]
+
+                   ["game"
+                    {:name :rc-game-page
+                     :view rc-game-page
+                     :controllers [{:start (fn [_]
+                                             (js/console.log "start rc-game-page"))
+                                    :stop (fn [_]
+                                            (js/console.log "stop rc-game-page"))}]}]
+                   ["setting"
+                    {:name :rc-settings-page
+                     :view rc-settings-page
+                     :controllers [{:start (fn [_]
+                                             (js/console.log "start rc-settings-page"))
+                                    :stop (fn [_]
+                                            (js/console.log "stop rc-settings-page"))}]}]]
+                  {:data {:controllers [{:start (fn [_]
+                                                  (js/console.log "start program"))
+                                         :stop (fn [_]
+                                                 (js/console.log "stop program"))}]
+                          :coercion Yzma.coercion.spec/coercion}})
+                 (fn [new-match]
+                   (swap! matchA (fn [old-match]
+                                   (if new-match
+                                     (assoc new-match :controllers (Yzma.frontend.controllers/apply-controllers (:controllers old-match) new-match))))))
+                 {:use-fragment false})]
+    (goog.events/unlistenByKey (:click-listen-key history))
+    (goog.events/listen js/document goog.events.EventType.CLICK
+                        (fn [event]
+                          (when-let [element (Yzma.frontend.history/closest-by-tag
+                                              (Yzma.frontend.history/event-target event) "a")]
+                            (let [uri (.parse goog.Uri (.-href element))]
+                              (when (Yzma.frontend.history/ignore-anchor-click? (.-router history) event element uri)
+                                (.preventDefault event)
+                                (let [path (str (.getPath uri)
+                                                (when (.hasQuery uri)
+                                                  (str "?" (.getQuery uri)))
+                                                (when (.hasFragment uri)
+                                                  (str "#" (.getFragment uri))))]
+                                  (.pushState js/window.history nil "" path)
+                                  (Yzma.frontend.history/-on-navigate history path)))))) true)
+
+
+    #_(Yzma.frontend.easy/push-state :rc-main-page)
+    (.render Pacha-dom-root (Kuzco.core/as-element [rc-current-page matchA stateA ops|]))))
 
 (defn -main
   []
@@ -38,10 +238,10 @@
     (println ":Madison oh my God, no - i dont even eat meat - i'm a vegatarian - vegan actually")
     #_(set! (.-innerHTML (.getElementById js/document "ui"))
             ":Co-Pilot i saw your planet destroyed - i was on the Death Star :_ which one?")
-    (Cara-Dune.kiwis/ui-process {:Pacha-dom-root Pacha-dom-root
-                                :matchA matchA
-                                :stateA stateA
-                                :ops| ops|})))
+    (ui-process {:Pacha-dom-root Pacha-dom-root
+                 :matchA matchA
+                 :stateA stateA
+                 :ops| ops|})))
 
 (defn reload
   []
