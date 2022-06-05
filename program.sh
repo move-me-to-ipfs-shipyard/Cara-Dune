ee1fd641764d3a7cc29a4695abd375e3162ba747
Cara-Dune <Cara-Dune@i-think-that-did-it>
2022-06-05 11:43:26 +0300

    i release electron-packager binaries for win macOS linux :Bill-Presindent-of-Hollywood-Hader   but things are looking up - you upcoming film that you wrote and directed   is an adaption of William-Falkner's As-i-lay-dying   well - i am very excited for that James and i must say - [snores]   it's gonna suck, James

--- program.sh
+++ program.sh
@@ -3,11 +3,15 @@
 
 install(){
   npm i --no-package-lock
+}
+
+copy(){
   mkdir -p out/jar/ui/
   mkdir -p out/jar/src/Cara-Dune/
   cp src/Cara_Dune/index.html out/jar/ui/index.html
   cp src/Cara_Dune/style.css out/jar/ui/style.css
   cp package.json out/jar/package.json
+  cp out/identicon/icon.png out/jar/icon.png
 }
 
 shadow(){
@@ -16,6 +20,7 @@ shadow(){
 
 repl(){
   install
+  copy
   shadow clj-repl
   # (shadow/watch :main)
   # (shadow/watch :ui)
@@ -23,31 +28,74 @@ repl(){
   # :repl/quit
 }
 
-jar(){
-
-  rm -rf out
-
+identicon(){
   clojure \
     -X:Zazu Zazu.core/process \
     :word '"Cara-Dune"' \
     :filename '"out/identicon/icon.png"' \
     :size 256
+}
+
+compile(){
+  shadow release :ui :main
+}
+
+out(){
+  rm -rf out
 
   install
-  cp out/identicon/icon.png out/jar/icon.png
-  shadow release :main :ui
-  # COMMIT_HASH=$(git rev-parse --short HEAD)
-  # COMMIT_COUNT=$(git rev-list --count HEAD)
-  # echo Cara-Dune-$COMMIT_COUNT-$COMMIT_HASH.zip
-  # cd out/jar
-  # zip -r ../Cara-Dune-$COMMIT_COUNT-$COMMIT_HASH.zip ./ && \
-  # cd ../../
+  identicon
+  copy
+}
+
+jar(){
+  COMMIT_HASH=$(git rev-parse --short HEAD)
+  COMMIT_COUNT=$(git rev-list --count HEAD)
+  echo Cara-Dune-$COMMIT_COUNT-$COMMIT_HASH.zip
+  cd out/jar
+  zip -r ../Cara-Dune-$COMMIT_COUNT-$COMMIT_HASH.zip ./ && \
+  cd ../../
+}
+
+package(){
+
+  convert out/identicon/icon.png -define icon:auto-resize=256,64,48,32,16 out/identicon/icon.ico
+  png2icns out/identicon/icon.icns out/identicon/icon.png
+  
+  # rm -rf out/zip
+
+  COMMIT_HASH=$(git rev-parse --short HEAD)
+  COMMIT_COUNT=$(git rev-list --count HEAD)
+  ln -s ../../node_modules out/jar/node_modules
+  npx electron-packager out/jar \
+    "Cara-Dune-$COMMIT_COUNT-$COMMIT_HASH" \
+    --overwrite \
+    --asar \
+    --executable-name=Cara-Dune \
+    --app-name=Cara-Dune \
+    --build-version="$COMMIT_COUNT-$COMMIT_HASH" \
+    --icon=out/identicon/icon.png \
+    --out=out/zip \
+    --platform=darwin,linux,win32 \
+    --arch=x64
+}
+
+zip_files(){
+  rm -rf out/zip/*.zip
+  for dir in out/zip/*; do
+    echo $dir
+    cd $dir
+    zip -qr ../../../$dir ./
+    cd ../../../
+  done
 }
 
 release(){
-  jar
+  out
+  compile
+  package
+  zip_files
 }
-
 
 tag(){
   COMMIT_HASH=$(git rev-parse --short HEAD)
