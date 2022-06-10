03408e22870655b474d2321545a09d1942b9d308
Cara-Dune <Cara-Dune@i-think-that-did-it>
2022-06-10 12:20:17 +0300

    i use jvm-libp2p - i remove aleph and ipfs-http-client :reporter is there a common thread you've noticed all these games in locker room at half time that causs them to come out at full tilt in third quarter? :Steve-Kerr just - i go into my book of incredibly inspiring quotes from movies and history and i just try to pull out the right one - i've no idea, it's been our pattern for many years, has nothing to with me or ciaching stuff

--- src/Cara_Dune/main.clj
+++ src/Cara_Dune/main.clj
@@ -13,12 +13,9 @@
    [clojure.repl]
    [cheshire.core]
 
-   [aleph.http]
-   [manifold.deferred]
-   [manifold.stream]
-   [byte-streams]
-
    [datahike.api]
+   
+   [Cara-Dune.libp2p]
 
    [Cara-Dune.seed]
    [Cara-Dune.raisins]
@@ -53,7 +50,6 @@
    (java.io File ByteArrayOutputStream PrintStream OutputStreamWriter PrintWriter)
    (java.lang Runnable)
 
-   (io.ipfs.api IPFS)
    (java.util.stream Stream)
    (java.util Base64)
    (java.io BufferedReader)
@@ -153,105 +149,6 @@
    '[Cara-Dune.rolled-oats]
    '[Cara-Dune.main]
    :reload))
-
-(defn encode-base64url-u
-  [^String string]
-  (-> (Base64/getUrlEncoder) (.withoutPadding)
-      (.encodeToString (.getBytes string StandardCharsets/UTF_8)) (->> (str "u"))))
-
-(defn decode-base64url-u
-  [^String string]
-  (-> (Base64/getUrlDecoder)
-      (.decode (subs string 1))
-      (String. StandardCharsets/UTF_8)))
-
-(defn pubsub-sub
-  [base-url topic message| cancel| raw-stream-connection-pool]
-  (let [streamV (volatile! nil)]
-    (->
-     (manifold.deferred/chain
-      (aleph.http/post (str base-url "/api/v0/pubsub/sub")
-                       {:query-params {:arg topic}
-                        :pool raw-stream-connection-pool})
-      :body
-      (fn [stream]
-        (vreset! streamV stream)
-        stream)
-      #(manifold.stream/map byte-streams/to-string %)
-      (fn [stream]
-        (manifold.deferred/loop
-         []
-          (->
-           (manifold.stream/take! stream :none)
-           (manifold.deferred/chain
-            (fn [message-string]
-              (when-not (identical? message-string :none)
-                (let [message (cheshire.core/parse-string message-string true)]
-                  #_(println :message message)
-                  (put! message| message))
-                (manifold.deferred/recur))))
-           (manifold.deferred/catch Exception (fn [ex] (println ex)))))))
-     (manifold.deferred/catch Exception (fn [ex] (println ex))))
-
-    (go
-      (<! cancel|)
-      (manifold.stream/close! @streamV))
-    nil))
-
-(defn pubsub-pub
-  [base-url topic message]
-  (let []
-
-    (->
-     (manifold.deferred/chain
-      (aleph.http/post (str base-url "/api/v0/pubsub/pub")
-                       {:query-params {:arg topic}
-                        :multipart [{:name "file" :content message}]})
-      :body
-      byte-streams/to-string
-      (fn [response-string] #_(println :repsponse reresponse-stringsponse)))
-     (manifold.deferred/catch
-      Exception
-      (fn [ex] (println ex))))
-
-    nil))
-
-(defn subscribe-process
-  [{:keys [^String ipfs-api-multiaddress
-           ^String ipfs-api-url
-           frequency
-           raw-stream-connection-pool
-           sub|
-           cancel|
-           id|]
-    :as opts}]
-  (let [ipfs (IPFS. ipfs-api-multiaddress)
-        base-url ipfs-api-url
-        topic (encode-base64url-u frequency)
-        id (-> ipfs (.id) (.get "ID"))
-        message| (chan (sliding-buffer 10))]
-    (put! id| {:peer-id id})
-    (pubsub-sub base-url  topic message| cancel| raw-stream-connection-pool)
-
-    (go
-      (loop []
-        (when-let [value (<! message|)]
-          (put! sub| (merge value
-                            {:message (-> (:data value) (decode-base64url-u) (read-string))}))
-          #_(println (merge value
-                            {:message (-> (:data value) (decode-base64url-u) (read-string))}))
-          #_(when-not (= (:from value) id)
-
-              #_(println (merge value
-                                {:message (-> (:data value) (decode-base64url-u) (read-string))})))
-          (recur))))
-
-    #_(go
-        (loop []
-          (<! (timeout 2000))
-          (pubsub-pub base-url topic (str {:id id
-                                           :rand-int (rand-int 100)}))
-          (recur)))))
 
 (defn menubar-process
   [{:keys [^JMenuBar jmenubar
@@ -940,153 +837,6 @@
   (.getName (class (make-array Object 1 1)))
 
   (.getName (class (make-array String 1)))
-
-  ;
-  )
-
-(comment
-
-  (go
-    (loop []
-      (when-let [{:keys [message from] :as value} (<! sub|)]
-        (condp = (:op message)
-          :game-state
-          (let [{:keys [game-state]} message]
-            (swap! gameA merge game-state))
-          :player-state
-          (let [{:keys [game-state]} message]
-            (swap! gameA update-in [:players from] merge message))
-          :games
-          (let [{:keys [frequency host-peer-id]} message]
-            (swap! gamesA update-in [frequency] merge message)))
-        (recur))))
-
-  (go
-    (loop []
-      (<! (timeout 3000))
-      (let [expired (into []
-                          (comp
-                           (keep (fn [[frequency {:keys [timestamp]}]]
-                                   #_(println (- (.getTime (java.util.Date.)) timestamp))
-                                   (when-not (< (- (.getTime (java.util.Date.)) timestamp) 4000)
-                                     frequency))))
-                          @gamesA)]
-        (when-not (empty? expired)
-          (apply swap! gamesA dissoc expired)))
-      (recur)))
-
-  (go
-    (loop []
-      (<! (timeout 3000))
-      (let [expired (into []
-                          (comp
-                           (keep (fn [[frequency {:keys [timestamp peer-id]}]]
-                                   #_(println (- (.getTime (java.util.Date.)) timestamp))
-                                   (when-not (< (- (.getTime (java.util.Date.)) timestamp) 4000)
-                                     frequency))))
-                          (:players @gameA))]
-        (when-not (empty? expired)
-          (apply swap! gameA update :players dissoc expired)))
-      (recur)))
-
-  (go
-    (loop []
-      (when-let [value (<! ops|)]
-        (condp = (:op value)
-          :game
-          (let [{:keys [frequency role]} value
-                id| (chan 1)
-                port (or (System/getenv "Jar_Jar_IPFS_PORT") "5001")
-                ipfs-api-url (format "http://127.0.0.1:%s" port)
-                games-topic (encode-base64url-u "raisins")
-                game-topic (encode-base64url-u frequency)
-                _ (subscribe-process
-                   {:sub| sub|
-                    :cancel| cancel-sub|
-                    :frequency frequency
-                    :ipfs-api-url ipfs-api-url
-                    :ipfs-api-multiaddress (format "/ip4/127.0.0.1/tcp/%s" port)
-                    :id| id|})
-                host? (= role :host)
-                {:keys [peer-id]} (<! id|)]
-            #_(println :game value)
-            (go
-              (loop []
-                (alt!
-                  cancel-pub|
-                  ([_] (do nil))
-
-                  (timeout 2000)
-                  ([_]
-                   (when host?
-                     (pubsub-pub
-                      ipfs-api-url games-topic (str {:op :games
-                                                     :timestamp (.getTime (java.util.Date.))
-                                                     :frequency frequency
-                                                     :host-peer-id peer-id}))
-                     (pubsub-pub
-                      ipfs-api-url game-topic (str {:op :game-state
-                                                    :timestamp (.getTime (java.util.Date.))
-                                                    :game-state {:host-peer-id peer-id}})))
-
-                   (pubsub-pub
-                    ipfs-api-url game-topic (str {:op :player-state
-                                                  :timestamp (.getTime (java.util.Date.))
-                                                  :peer-id peer-id}))
-                   (recur))))))
-
-          :leave
-          (let [{:keys [frequency]} value]
-            (>! cancel-sub| true)
-            (>! cancel-pub| true)
-            (reset! gameA {}))
-
-          :discover
-          (let [discover-jframe (JFrame. "discover")]
-            (discover-process
-             {:jframe discover-jframe
-              :root-jframe jframe
-              :ops| ops|
-              :gamesA gamesA
-              :gameA gameA
-              :stateA stateA})
-            (reset! gameA @gameA))
-
-          :settings
-          (let [settings-jframe (JFrame. "settings")]
-            (settings-process
-             {:jframe settings-jframe
-              :root-jframe jframe
-              :ops| ops|
-              :settingsA settingsA})
-            (reset! settingsA @settingsA))
-
-          :settings-value
-          (let []
-            (swap! settingsA merge value))
-
-          :host-yes
-          (let [{:keys [frequency]} value]
-            (println :frequency frequency)))
-
-        (recur))))
-
-
-
-  (let [port (or (System/getenv "Jar_Jar_IPFS_PORT") "5001")
-        ipfs-api-url (format "http://127.0.0.1:%s" port)
-        id| (chan 1)
-        raw-stream-connection-pool (aleph.http/connection-pool {:connection-options {:raw-stream? true}})]
-
-    (alter-var-root #'raw-stream-connection-pool (constantly raw-stream-connection-pool))
-    (subscribe-process
-     {:sub| sub|
-      :raw-stream-connection-pool raw-stream-connection-pool
-      :cancel| (chan (sliding-buffer 1))
-      :frequency "raisins"
-      :ipfs-api-url ipfs-api-url
-      :ipfs-api-multiaddress (format "/ip4/127.0.0.1/tcp/%s" port)
-      :id| id|}))
 
   ;
   )
