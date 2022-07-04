4099729ed6688b0ee5541cf524e79e8119760158
Cara-Dune <Cara-Dune@i-think-that-did-it>
2022-07-04 12:48:24 +0300

    my release is defined completely by commit hash :Bill-Burr people dont care about animated kids

--- program.sh
+++ program.sh
@@ -14,24 +14,13 @@ main(){
     -M -m Cara-Dune.main
 }
 
-tag(){
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
-  TAG="$COMMIT_COUNT-$COMMIT_HASH"
-  git tag $TAG $COMMIT_HASH
-  echo $COMMIT_HASH
-  echo $TAG
-}
-
 jar(){
 
   rm -rf out/*.jar out/classes
-  COMMIT_HASH=$(git rev-parse --short HEAD)
-  COMMIT_COUNT=$(git rev-list --count HEAD)
   clojure \
     -X:Genie Genie.core/process \
     :main-ns Cara-Dune.main \
-    :filename "\"out/Cara-Dune-$COMMIT_COUNT-$COMMIT_HASH.jar\"" \
+    :filename "\"out/Cara-Dune-$(git rev-parse --short HEAD).jar\"" \
     :paths '["src"]'
 }
 
